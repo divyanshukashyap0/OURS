42fc3f8ed851d20e55ffec8e9e13261c16d7a608
divyanshukashyap0 <divyanshu00884466@gmail.com>
2026-02-13 09:30:16 +0530

    Update

--- App.tsx
+++ App.tsx
@@ -61,7 +61,7 @@ const App: React.FC = () => {
 
               {/* Admin Routes */}
               <Route path="/admin" element={
-                <ProtectedRoute role="admin">
+                <ProtectedRoute>
                   <AdminLayout />
                 </ProtectedRoute>
               }>
